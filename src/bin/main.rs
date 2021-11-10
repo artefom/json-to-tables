533ea593b4360ba5547da4faf7b887ce1ae20cdf
Artyom Fomenko <artyom.fomenko@distribusion.com>
2021-11-10 12:16:52 +0300

    Added globbing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,7 @@ use std::io::BufReader;
 use std::path::PathBuf;
 use std::process::exit;
 
+use glob::glob;
 use json::object::Object;
 use structopt::StructOpt;
 
@@ -11,15 +12,17 @@ use json_to_tables;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "Json-to-tables",
-about = "Converts arbitrary jsons to line-saparated \
+about = "Converts arbitrary jsons to line-separated \
 json tables with foreign keys")]
 struct Cli {
+    /// Root name of converted table
+    name: String,
+
     /// Output directory path
     output: std::path::PathBuf,
 
     /// Source .json files to convert to file tables structure
-    #[structopt(parse(from_os_str))]
-    files: Vec<std::path::PathBuf>,
+    files: Vec<String>,
 }
 
 fn path_to_str(p: &PathBuf) -> String {
@@ -40,26 +43,39 @@ fn main() {
 
     let mut opened_files = HashMap::new();
 
+    // Define consumer to be used in parsing
+    // In this case, we want to consume to file directory structure
     let mut consumer = |table_name: &String, object: Object|
         json_to_tables::consumer_files::consume_to_files(&mut opened_files,
                                                          &opt.output,
                                                          table_name,
                                                          object).unwrap();
 
-    for path_buf in opt.files.iter() {
-        let file = File::open(path_buf.clone().as_path()).unwrap();
-        let mut reader = BufReader::new(file);
 
-        let mut handler = json_to_tables::parser::NestedObjectHandler::new(
-            String::from("root"), 0, &mut consumer);
-        let mut parser = yajlish::Parser::new(&mut handler);
+    for pattern in opt.files.iter() {
+        for entry in glob(pattern).expect("Failed to read glob pattern") {
+            match entry {
+                Ok(path_buf) => {
+                    let file = File::open(path_buf.clone().as_path()).unwrap();
+                    let mut reader = BufReader::new(file);
 
-        match parser.parse(&mut reader) {
-            Err(e) => {
-                println!("Error parsing {}: {}", path_to_str(path_buf), e)
+                    let mut handler = json_to_tables::parser::NestedObjectHandler::new(
+                        opt.name.clone(), 0, &mut consumer);
+                    let mut parser = yajlish::Parser::new(&mut handler);
+
+                    match parser.parse(&mut reader) {
+                        Err(e) => {
+                            println!("Error parsing {}: {}", path_to_str(&path_buf), e)
+                        }
+                        Ok(_) => { println!("Parsed {}", path_to_str(&path_buf)) }
+                    };
+                }
+                Err(e) => {
+                    println!("{:?}", e);
+                    exit(1)
+                }
             }
-            Ok(_) => { println!("Parsed {}", path_to_str(path_buf)) }
-        };
+        }
     }
 }
 
